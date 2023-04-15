3eaf960de7323854c1b9bb188e1b3f835b1d5920
Alexandre Floren;o <aflorenco13@gmail.com>
2023-04-15 16:26:34 -0300

    refactor: organization e Matrioshkas' animation

--- recursionApp/FirstView.swift
+++ recursionApp/FirstView.swift
@@ -8,66 +8,66 @@
 import SwiftUI
 
 struct FirstView: View {
-    var body: some View {
-        ZStack{
-            ZStack{
-                Image("background")
-                    .resizable()
-                    .ignoresSafeArea()
-                    .aspectRatio(contentMode: .fill)
-                    .offset(x: 570)
-                
-                ZStack{
-                    VStack{
-                        Spacer()
-                        
-                        Image("matryoshka")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 400)
-                    }
-                    
-                    VStack{
-                        Text("Welcome to the Recursion Tour!")
-                            .font(.custom("Apple SD Gothic Neo", size: 65))
-                            .padding(.bottom)
-                            .fontWeight(.bold)
-                        
-                        Text("Let's explore the concept of recursion, which is a very important programming technique used to solve problems that can be broken down into smaller subproblems.")
-                            .font(.custom("Apple SD Gothic Neo", size: 45))
-                            .fontWeight(.medium)
-                            .multilineTextAlignment(.center)
-                        
-                        Spacer()
-                        
-                    }
-                    .frame(width: 800, height: 600)
-                    .padding(.bottom, 200)
+    @State private var bouncing: Bool = false
     
-                    NavigationLink(destination: SecondView()){
-                        ButtonView(content: "Let's go!")
-                    }
-                }
-            }
-        }
-    }
-}
-
-struct ButtonView: View {
-    var content: String
     var body: some View {
-        Text(content)
-            .font(.custom("Apple SD Gothic Neo", size: 45))
-            .fontWeight(.black)
-            .foregroundColor(.white)
-            .frame(width: 245.0, height: 80)
-            .background(Color(red: 0.527, green: 0.841, blue: 0.408))
-            .cornerRadius(10)
-            .padding(.top, 200)
+        VStack{
+            Spacer()
+            
+            Text("Welcome to the Recursion Tour!")
+                .frame(width: 800, height: 200)
+                .font(.custom("Apple SD Gothic Neo", size: 70))
+                .fontWeight(.heavy)
+            
+            Text("Let's explore the concept of recursion, which is a very important programming technique used to solve problems that can be broken down into smaller subproblems.")
+                .frame(width: 800, height: 300, alignment: .center)
+                .multilineTextAlignment(.center)
+                .font(.custom("Apple SD Gothic Neo", size: 50))
+                .fontWeight(.medium)
+                .padding(.bottom, 40)
+            
+            NavigationLink(destination: SecondView()){
+                ButtonView(content: "Let's go!")
+            }
+            
+            HStack(alignment: .bottom){
+                Image("matryoshka 1")
+                    .frame(maxHeight: 450, alignment: .bottom)
+                    .offset(y: bouncing ? -50 : 0)
+                    .animation(Animation.easeInOut(duration: 0.5)
+                        .repeatForever()
+                        .delay(3), value: bouncing)
+                
+                    .onAppear {
+                        self.bouncing.toggle()
+                    }
+                Image("matryoshka 2")
+                    .frame(maxHeight: 450, alignment: .bottom)
+                    .offset(y: bouncing ? -50 : 0)
+                    .animation(Animation.easeInOut(duration: 0.5)
+                        .repeatForever()
+                        .delay(3.2), value: bouncing)
+                
+                    .onAppear {
+                        self.bouncing.toggle()
+                    }
+                Image("matryoshka 3")
+                    .frame(maxHeight: 450, alignment: .bottom)
+                    .offset(y: bouncing ? -50 : 0)
+                    .animation(Animation.easeInOut(duration: 0.5)
+                        .repeatForever()
+                        .delay(3.4), value: bouncing)
+                
+                    .onAppear {
+                        self.bouncing.toggle()
+                    }
+            }
+            .frame(width: 500)
+            
+        }
+        .background(BackgroundView())
     }
 }
-
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
