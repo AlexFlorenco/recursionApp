e700f1e43753a561d5098a911eaaa9b011ff1a30
Alexandre Floren;o <aflorenco13@gmail.com>
2023-04-16 10:57:08 -0300

    feat: Page layout

--- recursionApp/FourthView.swift
+++ recursionApp/FourthView.swift
@@ -63,90 +63,108 @@ struct FourthView: View {
         }
     }
     
-    @State var count = 0
+    @Environment(\.dismiss) private var dismiss
+    
+    @State var count = 1
     
     var body: some View {
         VStack{
-            ZStack{
-                if count >= 1{ // factorial(5)
-                    FactorialCardView(factorial: 5, code: codeView1(count) )
-                        .frame(width: 200)
+            Text("The Heap Recursive")
+                .frame(width: 800)
+                .padding(.top, 60)
+                .padding(.bottom, 30)
+                .font(.custom("Apple SD Gothic Neo", size: 60))
+                .fontWeight(.bold)
+            
+            
+            HStack{
+                Button{
+                    if count > 1{
+                        count -= 1
+                    }
+                    
+                } label: {
+                    if count > 1{
+                        StepButtonView(iconButton: "arrowshape.backward.fill", color: Color(red: 0.527, green: 0.841, blue: 0.408))
+                    }
+                    else {
+                        StepButtonView(iconButton: "arrowshape.backward.fill", color: Color(red: 0.950, green: 0.950, blue: 0.950))
+                    }
                 }
                 
-                if count >= 2 && count < 14{ // factorial(4)
-                    FactorialCardView(factorial: 4, code: codeView2(count) )
-                        .frame(width: 200)
-                        .offset(x: 50, y: 180)
+                Button{
+                    if count < 15{
+                    count += 1
+                    }
+                    
+                } label: {
+                    if count < 15{
+                        StepButtonView(iconButton: "arrowshape.forward.fill", color: Color(red: 0.527, green: 0.841, blue: 0.408))
+                    }
+                    else {
+                        StepButtonView(iconButton: "arrowshape.forward.fill", color: Color(red: 0.950, green: 0.950, blue: 0.950))
+                    }
                 }
-                
-                if count >= 3 && count < 12{ // factorial(3)
-                    FactorialCardView(factorial: 3, code: codeView3(count))
-                        .frame(width: 200)
-                        .offset(x: 100, y: 360)
-                }
-                
-                if count >= 4 && count < 10{ // factorial(2)
-                    FactorialCardView(factorial: 2, code: codeView4(count))
-                        .frame(width: 200)
-                        .offset(x: 150, y: 540)
-                }
-                
-                if count >= 5  && count < 8{ // factorial(1)
-                    FactorialCardView(factorial: 1, code: codeView5(count))
-                        .frame(width: 200)
-                        .offset(x: 200, y: 720)
-                }
-                
-                Spacer()
-                
             }
-            .frame(width: 200, height: 200)
-            .offset(x: -300)
+            .font(.largeTitle)
+            .padding(.bottom)
             
             Spacer()
-            Button{
-                count += 1
-                
-            } label: {
-                Text("ADD")
-                    .background(.red)
-                
-            }
-        }
-        .background(BackgroundView())
-    }
-}
 
-struct FactorialCardView: View {
-    var factorial: Int
-    var code: String
-    var body: some View{
-        ZStack{
-            Rectangle()
-                .foregroundColor(Color(red: 0.950, green: 0.950, blue: 0.950))
-            
-            Rectangle()
-                .strokeBorder(lineWidth: 2)
-            
-            VStack {
-                Text("factorial(\(factorial))")
-                    .font(.largeTitle)
-                    .padding(.top)
-                
-                Divider()
-                    .frame(height: 2)
-                    .background(.black)
-                
-                Spacer()
-                
-                Text("\(code)")
-                    .font(.title)
+            VStack{
+                ZStack{
+                    if count >= 1{ // factorial(5)
+                        FactorialCardView(factorial: 5, code: codeView1(count) )
+                            .frame(width: 200)
+                    }
+                    
+                    if count >= 2 && count < 14{ // factorial(4)
+                        FactorialCardView(factorial: 4, code: codeView2(count) )
+                            .frame(width: 200)
+                            .offset(x: 50, y: 180)
+                    }
+                    
+                    if count >= 3 && count < 12{ // factorial(3)
+                        FactorialCardView(factorial: 3, code: codeView3(count))
+                            .frame(width: 200)
+                            .offset(x: 100, y: 360)
+                    }
+                    
+                    if count >= 4 && count < 10{ // factorial(2)
+                        FactorialCardView(factorial: 2, code: codeView4(count))
+                            .frame(width: 200)
+                            .offset(x: 150, y: 540)
+                    }
+                    
+                    if count >= 5  && count < 8{ // factorial(1)
+                        FactorialCardView(factorial: 1, code: codeView5(count))
+                            .frame(width: 200)
+                            .offset(x: 200, y: 720)
+                    }
+                }
                 
                 Spacer()
             }
+            .frame(width: 800, height: 970)
+            .offset(x: -100)
+                        
+            Spacer()
             
+            HStack{
+                Button{
+                    dismiss()
+                } label: {
+                    PaginationButtonView(content: "Back")
+                }
+                
+                NavigationLink(destination: FourthView()){
+                    PaginationButtonView(content: "Next")
+                }
+            }
+            .padding(.bottom, 30)
         }
-        .aspectRatio(2/2, contentMode: .fit)
+        .navigationBarBackButtonHidden(true)
+        .background(BackgroundView())
     }
 }
 
